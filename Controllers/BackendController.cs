a3b9f1d22b6ca86d7031ed69781c5088c5a80da8
chenyekun1 <1181510942@qq.com>
2019-03-05 16:29:13 +0800

    Add backend create and edit function.

--- Controllers/BackendController.cs
+++ Controllers/BackendController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Session;
 using System;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using MovieTheater.Models;
 
 namespace MovieTheater.Controllers
 {
@@ -23,11 +24,42 @@ namespace MovieTheater.Controllers
         public IActionResult
         Create()
         {
-            ViewBag.movie_category_id = new SelectList(_context.MovieCategories, "CategoryId", "CategoryName");
-            ViewBag.movie_country_id  = new SelectList(_context.MovieCountries, "CountryId", "CountryName");
+            ViewBag.movie_category_id = _context.MovieCategories;
+            ViewBag.movie_country_id  = _context.MovieCountries;
             return View();
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult>
+        Create(int id,
+              [Bind("MovieId,MovieName," +
+                    "MovieActor," +
+                    "MovieDescription," +
+                    "MovieDirector," +
+                    "MovieGrade," +
+                    "MovieImg," +
+                    "MoviePrice," +
+                    "MovieCategoryId," +
+                    "MovieCountryId")]
+            Movie movie)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    await _context.AddAsync(movie);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(MovieManageIndex));
+                }
+            } catch (Exception)
+            {
+                Console.WriteLine("Update Error.");
+                throw;
+            }
+            return View(movie);
+        }
+        
         public async Task<IActionResult>
         MovieManageIndex(string sortOrder, string searchString)
         {
@@ -75,9 +107,42 @@ namespace MovieTheater.Controllers
             var movie = await _context.Movies.Include(m => m.movie_category)
                                              .Include(m => m.movie_country)
                                       .SingleOrDefaultAsync(m => m.MovieId == id);
-
+            ViewBag.movieCategory = _context.MovieCategories;
+            ViewBag.movieCountry  = _context.MovieCountries;
+            
             if (movie == null) return NotFound();
             return View(movie);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult>
+        MovieEdit(int id,
+                  [Bind("MovieId,MovieName," +
+                        "MovieActor," +
+                        "MovieDescription," +
+                        "MovieDirector," +
+                        "MovieGrade," +
+                        "MovieImg," +
+                        "MoviePrice," +
+                        "MovieCategoryId," +
+                        "MovieCountryId")]
+                  Movie movie)
+        {
+            if (id != movie.MovieId) return NotFound();
+            if (!ModelState.IsValid) return View(movie);
+            
+            try
+            {
+                _context.Update(movie);
+                await _context.SaveChangesAsync();
+            } catch (Exception ex)
+            {
+                if (!_context.Movies.Any(m => m.MovieId == movie.MovieId)) return NotFound();
+                else                                                       throw;
+            }
+
+            return RedirectToAction(nameof(MovieManageIndex));
         }
 
         public async Task<IActionResult>
@@ -111,7 +176,9 @@ namespace MovieTheater.Controllers
                 "Delete failed. Try again, and if the problem persists " +
                 "see your system administrator.";
             }
+
             throw new NotImplementedException();
+            //return View(movie);
         }
 
 
